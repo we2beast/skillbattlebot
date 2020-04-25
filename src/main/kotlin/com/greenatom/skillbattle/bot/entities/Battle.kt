350bd85633137ec89ae250df087fdc5a3e1e7f98
Daniil Vilyavin <89505105005@mail.ru>
2020-04-25 14:30:23 +0300

    Create Entity for surveys

---
+++ src/main/kotlin/com/greenatom/skillbattle/bot/entities/Battle.kt
@@ -0,0 +1,34 @@
+package com.greenatom.skillbattle.bot.entities
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "battles")
+data class Battle(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name = "battle_id")
+        var id: Int? = null,
+
+        @Column(name = "title")
+        var title: String? = null,
+
+        @Column(name = "description")
+        var description: String? = null,
+
+        @Column(name = "closed")
+        var closed: Boolean = false,
+
+        @Column(name = "start_date")
+        var startDate: LocalDateTime? = null,
+
+        @Column(name = "end_date")
+        var endDate: LocalDateTime? = null,
+
+        @Column(name = "speed")
+        var speed: Int? = null,
+
+        @Column(name = "started")
+        var started: Boolean = false
+)
---
+++ src/main/kotlin/com/greenatom/skillbattle/bot/entities/Gamer.kt
@@ -0,0 +1,19 @@
+package com.greenatom.skillbattle.bot.entities
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "gamers")
+data class Gamer(
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        @Column(name = "gamer_id")
+        var id: Int? = null,
+
+        @Column(name = "chat_id")
+        var chatId: String? = null,
+
+        @ManyToMany(cascade = [CascadeType.MERGE])
+        @JoinTable(name = "gamer_battle", joinColumns = [JoinColumn(name = "gamer_id")], inverseJoinColumns = [JoinColumn(name = "battle_id")])
+        var battles: Set<Battle> = setOf()
+)
